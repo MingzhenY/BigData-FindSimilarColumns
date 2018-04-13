16ce83401b1f96ea44c7fb61a1ed5191e24ac0f6
MingzhenY <31805779+MingzhenY@users.noreply.github.com>
2018-04-13 10:17:11 -0400

    Update gen.py
    Add functions to generate lists of strings

--- dis/gen.py
+++ dis/gen.py
@@ -8,14 +8,12 @@ def generateList(l,r):
     return ans
 
 
-
 def generateRandomList(n,l,r):
     '''generate a n-element list where each element is in range [l,r)'''
     ans = []
     for i in range(0,n):
         ans.append(random.randint(l,r))
     return ans
-
 
 
 def generateTestData_1(n,r):
@@ -32,8 +30,6 @@ def generateTestData_1(n,r):
     return (lx,ly)
 
 
-
-
 def repeatNList(n,l):
     '''repeat list l n times'''
     ans = []
@@ -41,4 +37,52 @@ def repeatNList(n,l):
         ans+=l
     return ans 
 
+
+def generateRandomString(L):
+    s = ''
+    for i in range(0,L):
+        r = random.randint(32,126)
+        s += chr(r)
+    return s
+
+
+def generateRandomStringList(n,L):
+    ret = []
+    for i in range(0,n):
+        ret.append(generateRandomString(L))
+    return ret
+
+
+def mutateString(s,k):
+    '''switch two adjacent chars in s at a time, for k times'''
+    l = len(s)
+    for i in range(0,k):
+        r = random.randint(0,l-2)
+        ns = s[0:r]+s[r+1]+s[r]+s[r+2:]
+        s = ns
+    return s
+
+
+def mutateStringList(ls,k):
+    ret = []
+    l = len(ls)
+    for s in ls:
+        ret.append(mutateString(s,k))
+    return (ls,ret)
+
+def generateTest_jaccardEx(n,L,k,sim):
+    '''
+    n string of length L
+    where n*sim are mutated k times
+          and n - n*sim are completely different
+    '''
+    nsim = int(n*sim)
+    if nsim < 0 :
+        nsim = 0 
+    if nsim > n :
+        nsim = n
+    ls,rs = mutateStringList(generateRandomStringList(nsim,L),k)
+    ls += generateRandomStringList(n-nsim,L)
+    rs += generateRandomStringList(n-nsim,L)
+    return (ls,rs)
 
