3bb9b40f565f45ca25af5b617629b24b0e2bf40f
MingzhenY <31805779+MingzhenY@users.noreply.github.com>
2018-04-13 00:32:41 -0400

    Update dis.py

--- dis/dis.py
+++ dis/dis.py
@@ -1,14 +1,15 @@
 def jaccard_bag(x,y):
-   """calculate jaccard distance of x and y (x and y are rdds)"""
-    u = x.subtract(y).count()+y.subtract(x).count()
+    """calculate jaccard distance of x and y (x and y are rdds)"""
+    u = x.subtract(y).count() + y.subtract(x).count()
     v = x.union(y).count()
-    return 1.0 - u * 1.0 / v
+    return u * 1.0 / v
 
 def jaccard_set(x,y):
     '''eliminate duplicate before calculating jaccard distance of x and y'''
     dx = x.distinct()
     dy = y.distinct()
-    u = dx.subtract(dy).count()+dy.subtract(dx).count()
-    v = dx.union(dy).count()
+    u = dx.intersection(dy).count()
+    v = dx.union(dy).distinct().count()
     return 1.0 - u * 1.0 / v
+
 
