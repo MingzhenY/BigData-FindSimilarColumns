a2fb41a5be1f4ace9a1dfe397c5d6ab6a4a3e916
MingzhenY <31805779+MingzhenY@users.noreply.github.com>
2018-05-14 10:24:42 -0400

    Update LSH.py

--- LSH.py
+++ LSH.py
@@ -237,7 +237,12 @@ def getNormCols(cols,q):
 
 def timeTest(cols,n,normalize = True):
     '''
-    q = 4, (b, r) = (100, 2) , s >= 0.1, DS = old (1199)
+    Note: 
+        'sig.size' indicates how many columns is sutable to use this method.
+        If all strings are shorter than q characters, then q-gram will not work.
+        In this case the column is ignored.
+    
+    q = 4, (b, r) = (100, 2) , s >= 0.1, DS = old (1199 columns)
     n       time        sig.size    bucket.size     pairs.size
     10      12.12s      5           451             1
     100     359.07s     38          3065            24
@@ -245,23 +250,23 @@ def timeTest(cols,n,normalize = True):
     1200    11498.91s   551         33215           4183
     
     
-    q = 4, (b, r) = (20, 2) , s >= 0.22   DS = old (975)
+    q = 4, (b, r) = (20, 2) , s >= 0.22   DS = old (975 columns)
     n       time        sig.size    bucket.size     pairs.size
     100     73s         31          564             10
     200     294s        68          1172            87
     
-    q = 6, (b, r) = (20, 7) , s >= 0.65   DS = old (975)
+    q = 6, (b, r) = (20, 7) , s >= 0.65   DS = old (975 columns)
     n       time        sig.size    bucket.size     pairs.size
     100     176s        23          420             3
     200     450s        53          997             7
     500     879s        153         2671            110
     1000    1846s       299         5050            489
     
-    q = 5, (b, r) = (20, 7) , s >= 0.65,  DS = new (379)
+    q = 5, (b, r) = (20, 7) , s >= 0.65,  DS = new (379 columns)
     n       time        sig.size    bucket.size     pairs.size
     379     699s        218         4204            14
     
-    q = 4, (b, r) = (20, 2) , s >= 0.22,  DS = new (379)
+    q = 4, (b, r) = (20, 2) , s >= 0.22,  DS = new (379 columns)
     n       time        sig.size    bucket.size     pairs.size
     379     1312        257         4125            401
     '''
@@ -280,41 +285,15 @@ def showKey(data,key):
     data[key[1][0]].select(key[1][1]).distinct().where(col(key[1][1]).isNotNull()).orderBy(col(key[1][1])).show()
 
 
-def filter_pairs(pairs):
-    ret = {}
-    filename = {}
-    for key in pairs:
-        left = '%ELL ' in key[0][1] or '#ELL ' in key[0][1] \
-            or '#Grade' in key[0][1]
-        right = '%ELL ' in key[1][1] or '#ELL ' in key[1][1] \
-            or '#Grade' in key[1][1]
-        if  left and right:
-            #filename[key[0][0]] = True
-            #filename[key[1][0]] = True
-            filename[key[1]] = True
-            filename[key[0]] = True
-            pass 
-        else:
-            ret[key] = pairs[key]
-    print(filename)
-    return ret
-
-
-normCols_6 = getNormCols(cols,6)
-pairs = timeTest(normCols_6,100,False)
-
-pairs = timeTest(cols,503)
-fpairs = filter_pairs(pairs)
-
 def test(n):
     key = list(pairs.keys())[n]
     print(key)
     showKey(data,key)
 
 
-#FindSimilarColumns_LSH(sample_cols,20,7,0.65,5,normalize)
+#FindSimilarColumns_LSH(cols,20,7,0.65,5)
 pairs = {(('2fws-68t6.json', 'Service HIV/AIDS'), ('2fws-68t6.json', 'Specialize HIV/AIDS')): 0.0, (('2fws-68t6.json', 'Service Elder Abuse'), ('2fws-68t6.json', 'Specialize Elder Abuse Services')): 0.25, (('27h8-t3wt.json', 'Category'), ('yu9n-iqyk.json', 'Category')): 0.0, (('2fws-68t6.json', 'Specialize Batterers'), ('2fws-68t6.json', 'Specialize Male Survivors')): 0.3333333333333333, (('2fws-68t6.json', 'Service Stalking Victim Services '), ('2fws-68t6.json', 'Specialize Services Stalking Victims ')): 0.0, (('3mrr-8h5c.json', 'DBN'), ('yu9n-iqyk.json', 'DBN')): 0.008719346049046322, (('yhuu-4pt3.json', 'Hack Up Date'), ('yhuu-4pt3.json', 'Suspension Date')): 0.1829004329004329, (('27h8-t3wt.json', 'Category'), ('3mrr-8h5c.json', 'Demographic')): 0.0, (('yhuu-4pt3.json', 'Hack Up Date'), ('yjub-udmw.json', 'Activated')): 0.34702702702702704, (('yhuu-4pt3.json', 'Certification Date'), ('yhuu-4pt3.json', 'Suspension Date')): 0.1815217391304348, (('yhuu-4pt3.json', 'Certification Date'), ('yhuu-4pt3.json', 'Hack Up Date')): 0.006493506493506494, (('yhuu-4pt3.json', 'Certification Date'), ('yjub-udmw.json', 'Activated')): 0.34276387377584333, (('2zbg-i8fx.json', 'COMPARABLE RENTAL – 1 – Building Classification'), ('2zbg-i8fx.json', 'COMPARABLE RENTAL – 2 – Building Classification')): 0.21052631578947367, (('3mrr-8h5c.json', 'Demographic'), ('yu9n-iqyk.json', 'Category')): 0.0}
 
 
-
-fpairs = {(('2fws-68t6.json', 'Specialize Batterers'), ('2fws-68t6.json', 'Specialize Male Survivors')): 0.3076923076923077, (('xck4-5xd5.json', 'NTA'), ('yjub-udmw.json', 'NTAName')): 0.19181479950392724, (('29ry-u5bf.json', 'DBN'), ('2x8v-d8nh.json', 'DBN')): 0.34421534936998854, (('25aa-q86c.json', 'DBN'), ('yu9n-iqyk.json', 'DBN')): 0.1755361397934869, (('25aa-q86c.json', 'School Name'), ('29ry-u5bf.json', 'School Name')): 0.03399722769387904, (('25aa-q86c.json', 'Category'), ('29ry-u5bf.json', 'Category')): 0.23357664233576642, (('2x8v-d8nh.json', 'DBN'), ('xqmg-7z3j.json', 'ATS Code')): 0.06934097421203439, (('xqmg-7z3j.json', 'Location Name'), ('yeba-ynb5.json', 'LOCATION NAME')): 0.057243255497619584, (('25aa-q86c.json', 'School Name'), ('2x8v-d8nh.json', 'LOCATION NAME')): 0.2213696417238793, (('29ry-u5bf.json', 'DBN'), ('yeba-ynb5.json', 'DBN')): 0.34421534936998854, (('2x8v-d8nh.json', 'LOCATION CATEGORY'), ('yeba-ynb5.json', 'LOCATION CATEGORY')): 0.12857142857142856, (('29ry-u5bf.json', 'School Name'), ('yeba-ynb5.json', 'LOCATION NAME')): 0.23984669946230408, (('3mrr-8h5c.json', 'DBN'), ('yu9n-iqyk.json', 'DBN')): 0.007894736842105263, (('yhuu-4pt3.json', 'Certification Date'), ('yhuu-4pt3.json', 'Suspension Date')): 0.2105997210599721, (('29ry-u5bf.json', 'School Name'), ('xqmg-7z3j.json', 'Location Name')): 0.21486875330318866, (('2fws-68t6.json', 'Service Elder Abuse'), ('2fws-68t6.json', 'Specialize Elder Abuse Services')): 0.2608695652173913, (('27h8-t3wt.json', 'Category'), ('yu9n-iqyk.json', 'Category')): 0.0, (('27h8-t3wt.json', 'Category'), ('3mrr-8h5c.json', 'Demographic')): 0.0, (('25aa-q86c.json', 'School Name'), ('yeba-ynb5.json', 'LOCATION NAME')): 0.22127978766372397, (('29bv-qqsy.json', 'LOCATION NAME'), ('yeba-ynb5.json', 'LOCATION NAME')): 0.0, (('xck4-5xd5.json', 'borough'), ('yjub-udmw.json', 'BoroName')): 0.0, (('29bv-qqsy.json', 'LOCATION CATEGORY'), ('yeba-ynb5.json', 'LOCATION CATEGORY')): 0.0, (('29ry-u5bf.json', 'School Name'), ('2x8v-d8nh.json', 'LOCATION NAME')): 0.2399336536261725, (('yhuu-4pt3.json', 'Certification Date'), ('yhuu-4pt3.json', 'Hack Up Date')): 0.009002770083102494, (('2fws-68t6.json', 'Service Stalking Victim Services '), ('2fws-68t6.json', 'Specialize Services Stalking Victims ')): 0.0, (('29bv-qqsy.json', 'LOCATION NAME'), ('xqmg-7z3j.json', 'Location Name')): 0.057243255497619584, (('25aa-q86c.json', 'DBN'), ('yeba-ynb5.json', 'DBN')): 0.332378223495702, (('25aa-q86c.json', 'DBN'), ('29ry-u5bf.json', 'DBN')): 0.01970865467009426, (('2x8v-d8nh.json', 'LOCATION NAME'), ('xqmg-7z3j.json', 'Location Name')): 0.05735010767312705, (('xqmg-7z3j.json', 'ATS Code'), ('yeba-ynb5.json', 'DBN')): 0.06934097421203439, (('29bv-qqsy.json', 'LOCATION NAME'), ('29ry-u5bf.json', 'School Name')): 0.23984669946230408, (('25aa-q86c.json', 'School Name'), ('xqmg-7z3j.json', 'Location Name')): 0.2130493457724579, (('29ry-u5bf.json', 'DBN'), ('3mrr-8h5c.json', 'DBN')): 0.19081551860649248, (('29bv-qqsy.json', 'LOCATION CATEGORY'), ('2x8v-d8nh.json', 'LOCATION CATEGORY')): 0.12857142857142856, (('29bv-qqsy.json', 'DBN'), ('2x8v-d8nh.json', 'DBN')): 0.0, (('29bv-qqsy.json', 'DBN'), ('xqmg-7z3j.json', 'ATS Code')): 0.06934097421203439, (('29ry-u5bf.json', 'DBN'), ('yu9n-iqyk.json', 'DBN')): 0.19349722442505948, (('2zbg-i8fx.json', 'COMPARABLE RENTAL – 1 – Building Classification'), ('2zbg-i8fx.json', 'COMPARABLE RENTAL – 2 – Building Classification')): 0.23529411764705882, (('3mrr-8h5c.json', 'DBN'), ('xqmg-7z3j.json', 'ATS Code')): 0.34054054054054056, (('25aa-q86c.json', 'DBN'), ('xqmg-7z3j.json', 'ATS Code')): 0.2868550368550369, (('29bv-qqsy.json', 'LOCATION NAME'), ('2x8v-d8nh.json', 'LOCATION NAME')): 0.00103836169599077, (('2fws-68t6.json', 'Service HIV/AIDS'), ('2fws-68t6.json', 'Specialize HIV/AIDS')): 0.0, (('25aa-q86c.json', 'DBN'), ('29bv-qqsy.json', 'DBN')): 0.332378223495702, (('29ry-u5bf.json', 'DBN'), ('xqmg-7z3j.json', 'ATS Code')): 0.2995702885205648, (('25aa-q86c.json', 'DBN'), ('2x8v-d8nh.json', 'DBN')): 0.332378223495702, (('xqmg-7z3j.json', 'ATS Code'), ('yu9n-iqyk.json', 'DBN')): 0.3457382953181273, (('2x8v-d8nh.json', 'LOCATION NAME'), ('yeba-ynb5.json', 'LOCATION NAME')): 0.00103836169599077, (('29bv-qqsy.json', 'DBN'), ('yeba-ynb5.json', 'DBN')): 0.0, (('yhuu-4pt3.json', 'Hack Up Date'), ('yhuu-4pt3.json', 'Suspension Date')): 0.21398891966759004, (('25aa-q86c.json', 'DBN'), ('3mrr-8h5c.json', 'DBN')): 0.17287866772402855, (('25aa-q86c.json', 'School Name'), ('29bv-qqsy.json', 'LOCATION NAME')): 0.22127978766372397, (('2x8v-d8nh.json', 'DBN'), ('yeba-ynb5.json', 'DBN')): 0.0, (('29bv-qqsy.json', 'DBN'), ('29ry-u5bf.json', 'DBN')): 0.34421534936998854, (('3mrr-8h5c.json', 'Demographic'), ('yu9n-iqyk.json', 'Category')): 0.0}
+#Old results
+pairs = {(('2fws-68t6.json', 'Specialize Batterers'), ('2fws-68t6.json', 'Specialize Male Survivors')): 0.3076923076923077, (('xck4-5xd5.json', 'NTA'), ('yjub-udmw.json', 'NTAName')): 0.19181479950392724, (('29ry-u5bf.json', 'DBN'), ('2x8v-d8nh.json', 'DBN')): 0.34421534936998854, (('25aa-q86c.json', 'DBN'), ('yu9n-iqyk.json', 'DBN')): 0.1755361397934869, (('25aa-q86c.json', 'School Name'), ('29ry-u5bf.json', 'School Name')): 0.03399722769387904, (('25aa-q86c.json', 'Category'), ('29ry-u5bf.json', 'Category')): 0.23357664233576642, (('2x8v-d8nh.json', 'DBN'), ('xqmg-7z3j.json', 'ATS Code')): 0.06934097421203439, (('xqmg-7z3j.json', 'Location Name'), ('yeba-ynb5.json', 'LOCATION NAME')): 0.057243255497619584, (('25aa-q86c.json', 'School Name'), ('2x8v-d8nh.json', 'LOCATION NAME')): 0.2213696417238793, (('29ry-u5bf.json', 'DBN'), ('yeba-ynb5.json', 'DBN')): 0.34421534936998854, (('2x8v-d8nh.json', 'LOCATION CATEGORY'), ('yeba-ynb5.json', 'LOCATION CATEGORY')): 0.12857142857142856, (('29ry-u5bf.json', 'School Name'), ('yeba-ynb5.json', 'LOCATION NAME')): 0.23984669946230408, (('3mrr-8h5c.json', 'DBN'), ('yu9n-iqyk.json', 'DBN')): 0.007894736842105263, (('yhuu-4pt3.json', 'Certification Date'), ('yhuu-4pt3.json', 'Suspension Date')): 0.2105997210599721, (('29ry-u5bf.json', 'School Name'), ('xqmg-7z3j.json', 'Location Name')): 0.21486875330318866, (('2fws-68t6.json', 'Service Elder Abuse'), ('2fws-68t6.json', 'Specialize Elder Abuse Services')): 0.2608695652173913, (('27h8-t3wt.json', 'Category'), ('yu9n-iqyk.json', 'Category')): 0.0, (('27h8-t3wt.json', 'Category'), ('3mrr-8h5c.json', 'Demographic')): 0.0, (('25aa-q86c.json', 'School Name'), ('yeba-ynb5.json', 'LOCATION NAME')): 0.22127978766372397, (('29bv-qqsy.json', 'LOCATION NAME'), ('yeba-ynb5.json', 'LOCATION NAME')): 0.0, (('xck4-5xd5.json', 'borough'), ('yjub-udmw.json', 'BoroName')): 0.0, (('29bv-qqsy.json', 'LOCATION CATEGORY'), ('yeba-ynb5.json', 'LOCATION CATEGORY')): 0.0, (('29ry-u5bf.json', 'School Name'), ('2x8v-d8nh.json', 'LOCATION NAME')): 0.2399336536261725, (('yhuu-4pt3.json', 'Certification Date'), ('yhuu-4pt3.json', 'Hack Up Date')): 0.009002770083102494, (('2fws-68t6.json', 'Service Stalking Victim Services '), ('2fws-68t6.json', 'Specialize Services Stalking Victims ')): 0.0, (('29bv-qqsy.json', 'LOCATION NAME'), ('xqmg-7z3j.json', 'Location Name')): 0.057243255497619584, (('25aa-q86c.json', 'DBN'), ('yeba-ynb5.json', 'DBN')): 0.332378223495702, (('25aa-q86c.json', 'DBN'), ('29ry-u5bf.json', 'DBN')): 0.01970865467009426, (('2x8v-d8nh.json', 'LOCATION NAME'), ('xqmg-7z3j.json', 'Location Name')): 0.05735010767312705, (('xqmg-7z3j.json', 'ATS Code'), ('yeba-ynb5.json', 'DBN')): 0.06934097421203439, (('29bv-qqsy.json', 'LOCATION NAME'), ('29ry-u5bf.json', 'School Name')): 0.23984669946230408, (('25aa-q86c.json', 'School Name'), ('xqmg-7z3j.json', 'Location Name')): 0.2130493457724579, (('29ry-u5bf.json', 'DBN'), ('3mrr-8h5c.json', 'DBN')): 0.19081551860649248, (('29bv-qqsy.json', 'LOCATION CATEGORY'), ('2x8v-d8nh.json', 'LOCATION CATEGORY')): 0.12857142857142856, (('29bv-qqsy.json', 'DBN'), ('2x8v-d8nh.json', 'DBN')): 0.0, (('29bv-qqsy.json', 'DBN'), ('xqmg-7z3j.json', 'ATS Code')): 0.06934097421203439, (('29ry-u5bf.json', 'DBN'), ('yu9n-iqyk.json', 'DBN')): 0.19349722442505948, (('2zbg-i8fx.json', 'COMPARABLE RENTAL – 1 – Building Classification'), ('2zbg-i8fx.json', 'COMPARABLE RENTAL – 2 – Building Classification')): 0.23529411764705882, (('3mrr-8h5c.json', 'DBN'), ('xqmg-7z3j.json', 'ATS Code')): 0.34054054054054056, (('25aa-q86c.json', 'DBN'), ('xqmg-7z3j.json', 'ATS Code')): 0.2868550368550369, (('29bv-qqsy.json', 'LOCATION NAME'), ('2x8v-d8nh.json', 'LOCATION NAME')): 0.00103836169599077, (('2fws-68t6.json', 'Service HIV/AIDS'), ('2fws-68t6.json', 'Specialize HIV/AIDS')): 0.0, (('25aa-q86c.json', 'DBN'), ('29bv-qqsy.json', 'DBN')): 0.332378223495702, (('29ry-u5bf.json', 'DBN'), ('xqmg-7z3j.json', 'ATS Code')): 0.2995702885205648, (('25aa-q86c.json', 'DBN'), ('2x8v-d8nh.json', 'DBN')): 0.332378223495702, (('xqmg-7z3j.json', 'ATS Code'), ('yu9n-iqyk.json', 'DBN')): 0.3457382953181273, (('2x8v-d8nh.json', 'LOCATION NAME'), ('yeba-ynb5.json', 'LOCATION NAME')): 0.00103836169599077, (('29bv-qqsy.json', 'DBN'), ('yeba-ynb5.json', 'DBN')): 0.0, (('yhuu-4pt3.json', 'Hack Up Date'), ('yhuu-4pt3.json', 'Suspension Date')): 0.21398891966759004, (('25aa-q86c.json', 'DBN'), ('3mrr-8h5c.json', 'DBN')): 0.17287866772402855, (('25aa-q86c.json', 'School Name'), ('29bv-qqsy.json', 'LOCATION NAME')): 0.22127978766372397, (('2x8v-d8nh.json', 'DBN'), ('yeba-ynb5.json', 'DBN')): 0.0, (('29bv-qqsy.json', 'DBN'), ('29ry-u5bf.json', 'DBN')): 0.34421534936998854, (('3mrr-8h5c.json', 'Demographic'), ('yu9n-iqyk.json', 'Category')): 0.0}
